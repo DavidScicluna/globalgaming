91fc4350af224165cadcdd32cef166f8d211f4bd
DavidScicluna\DavidScicluna <david.scicluna.a106106@mcast.edu.mt>
2020-04-21 13:24:29 +0200

    Updates

---
+++ precache-manifest.640dad01a2222d5a5756c8ff3abb8ba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7605706cec9bcbad7819fb566bba5ce8",
+    "url": "/globalgaming/index.html"
+  },
+  {
+    "revision": "fc6eab74e0270c212b24",
+    "url": "/globalgaming/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d38d5f35317424f1568",
+    "url": "/globalgaming/static/js/2.a982d090.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/globalgaming/static/js/2.a982d090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6eab74e0270c212b24",
+    "url": "/globalgaming/static/js/main.04ce03ee.chunk.js"
+  },
+  {
+    "revision": "8a835dc8ba30ed98ee13",
+    "url": "/globalgaming/static/js/runtime-main.b18b6b02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/globalgaming/static/media/logo.5d5d9eef.svg"
+  }
+]);
